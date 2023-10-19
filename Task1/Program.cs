452b62ca4c7b9c2ee158f263dea487509e3f7fc5
Neddy <Ned@gmail.com>
2023-10-19 15:57:14 +0300

    Task_1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int minNumber = 100;
+int maxNumber = 999;
+
+int number = new Random().Next(minNumber, maxNumber + 1);
+string numberString = Convert.ToString(number);
+
+char secondChar = numberString[1];
+
+System.Console.WriteLine($"{numberString} -> {secondChar}");
